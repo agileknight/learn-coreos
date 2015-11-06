21813f296b4dd39a8d4b4ee0b06839d1699616de
Philipp Meier <philipp.meier@codecentric.de>
2015-11-06 13:30:03 +0100

    use beta instead of stable so kubelet is pre-installed

--- create.sh
+++ create.sh
@@ -11,7 +11,7 @@ curl --request POST "https://api.digitalocean.com/v2/droplets" \
      --header "Content-Type: application/json" \
      --header "Authorization: Bearer $TOKEN" \
      --data '{"region":"'"${REGION}"'",
-        "image":"coreos-stable",
+        "image":"coreos-beta",
 	"private_networking":true,
         "size":"'"$SIZE"'",
         "user_data": "'"$(cat cloud-config)"'",
